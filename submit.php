559100d34886fb0f1e348d32e3bf227661f2630e
AnisahM <74705108+AnisahM@users.noreply.github.com>
2020-12-28 02:28:48 +0800

    Update submit.php

--- submit.php
+++ submit.php
@@ -13,9 +13,9 @@ Jumlah Kehadiran : ".$_POST['bil']."
 
 <script type="text/javascript">
 	if (screen.width > 768)
-		setTimeout(window.location.href = "https://web.whatsapp.com/send?phone=<?php echo $_GET['no'] ?>&text=<?php echo urlencode ( $str ); ?>",700);
+		setTimeout(window.location.href = "https://web.whatsapp.com/send?phone=60145599160&text=",700);
 	else 
-		setTimeout(window.location.href = "https://api.whatsapp.com/send?phone=<?php echo $_GET['no'] ?>&text=<?php echo urlencode ( $str ); ?>",700);
+		setTimeout(window.location.href = "https://api.whatsapp.com/send?phone=60145599160&text=",700);
 
 </script>
 
