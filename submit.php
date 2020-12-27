791ee253ab5080c55006baee2f9d581215d8de74
AnisahM <74705108+AnisahM@users.noreply.github.com>
2020-12-28 02:39:13 +0800

    Update submit.php

--- submit.php
+++ submit.php
@@ -4,6 +4,7 @@
 <?php
 $str="
 Nama : ".$_POST['famName']."
+Nombor Telefon : ".$_POST['no']."
 Jumlah Kehadiran : ".$_POST['bil']."
 ";
 
@@ -13,9 +14,9 @@ Jumlah Kehadiran : ".$_POST['bil']."
 
 <script type="text/javascript">
 	if (screen.width > 768)
-		setTimeout(window.location.href = "https://web.whatsapp.com/send?phone=60145599160&text=",700);
+		setTimeout(window.location.href = "https://web.whatsapp.com/send?phone=<?php echo $_GET['no'] ?>&text=<?php echo urlencode ( $str ); ?>",700);
 	else 
-		setTimeout(window.location.href = "https://api.whatsapp.com/send?phone=60145599160&text=",700);
+		setTimeout(window.location.href = "https://api.whatsapp.com/send?phone=<?php echo $_GET['no'] ?>&text=<?php echo urlencode ( $str ); ?>",700);
 
 </script>
 
