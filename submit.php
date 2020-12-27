bacaacc777aaa5ef9357e7b22dc82759345abfb5
AnisahM <74705108+AnisahM@users.noreply.github.com>
2020-12-28 02:23:40 +0800

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+
+<?php
+$str="
+Nama : ".$_POST['famName']."
+Jumlah Kehadiran : ".$_POST['bil']."
+";
+
+
+
+?>
+
+<script type="text/javascript">
+	if (screen.width > 768)
+		setTimeout(window.location.href = "https://web.whatsapp.com/send?phone=<?php echo $_GET['no'] ?>&text=<?php echo urlencode ( $str ); ?>",700);
+	else 
+		setTimeout(window.location.href = "https://api.whatsapp.com/send?phone=<?php echo $_GET['no'] ?>&text=<?php echo urlencode ( $str ); ?>",700);
+
+</script>
+
+</body>
+</html>
